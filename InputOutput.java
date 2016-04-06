281f158b25fb72e6456d6f454ef71771e0beb8eb
mxs744 <mxs744@gmail.com>
2016-04-06 14:19:04 -0400

    Added files via upload

---
+++ InputOutput.java
@@ -0,0 +1,28 @@
+/** 
+ *Author: Michael Schreiber CSC 200
+ *Date: April 6, 2016
+ *Contact Email: mxs744@gmail.com
+ *Assignment 2 Extra Credit InPutOutput: This program displays output information
+ * @author ms22472
+ *
+ */
+
+public class InputOutput {
+	public static void main(String[] args){
+		
+		
+		double answer = ((3.5*5)/4);
+		double answer2 = 3.5*(5/4);
+		
+		System.out.println("1/2 is " + 1/2 + " Integer value");
+		System.out.println("1%2 is " + 1%2 + " Integer value " );
+		System.out.println("1.0/2 is " + 1.0/2 + " Double Value");
+		System.out.println("5+7/2 is " + 5+7/2 + " Integer Value");
+		System.out.println("Beat" + "" + "Army" + " String Value");
+		System.out.println("3.5*(5/4) is " + answer2 + " Double Value");
+		System.out.println("(3.5*5)/4 is " + answer + " Double Value");
+			
+		
+	}
+}
+
